a20c3bef4180222105f8967bfe1efaafd96c90b9
tweetpsa <tweetpsa@gmail.com>
2020-05-11 11:32:36 +0800

    TWEETPSA.IO 2.0

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,2 +1,10 @@
 $("#menu").load("./assets/include/menu.html");
 $("#footer").load("./assets/include/footer.html");
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+});
+
+function myTweet() {
+  var x = document.getElementById("myText").value;
+  window.open('https://twitter.com/intent/tweet?&text=' + x , "_blank");
+}
